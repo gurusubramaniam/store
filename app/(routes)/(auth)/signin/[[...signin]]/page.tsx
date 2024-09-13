b5355b6b2f3d18869a3b7de5a77ff83404a072d6
Gurusubramaniam <gurutii@gmail.com>
2024-09-13 14:22:24 -0400

    fix build error

--- app/(routes)/(auth)/signin/[[...signin]]/page.tsx
+++ app/(routes)/(auth)/signin/[[...signin]]/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { SignIn } from '@clerk/nextjs'
-import { Sign } from 'crypto'
 export default function Signin () {
   return(
   <div className="flex justify-center">
